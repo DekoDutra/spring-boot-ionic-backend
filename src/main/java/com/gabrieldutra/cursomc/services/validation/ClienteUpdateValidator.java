adf613b202787754dcd3c0197fbb660619887109
Gabriel Dutra <gabrielhenriquedutra@gmail.com>
2018-07-18 20:18:48 -0300

    Validacao de email PUT

---
+++ src/main/java/com/gabrieldutra/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,54 @@
+package com.gabrieldutra.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.gabrieldutra.cursomc.domain.Cliente;
+import com.gabrieldutra.cursomc.dto.ClienteDTO;
+import com.gabrieldutra.cursomc.repositories.ClienteRepository;
+import com.gabrieldutra.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
