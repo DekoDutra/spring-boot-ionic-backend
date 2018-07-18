9c9ce182f2311cf67573b499080500096eabb419
Gabriel Dutra <gabrielhenriquedutra@gmail.com>
2018-07-18 16:23:17 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/gabrieldutra/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrieldutra/cursomc/services/CategoriaService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.gabrieldutra.cursomc.domain.Categoria;
 import com.gabrieldutra.cursomc.repositories.CategoriaRepository;
 import com.gabrieldutra.cursomc.services.exceptions.DataIntegrityException;
 import com.gabrieldutra.cursomc.services.exceptions.ObjectNotFoundException;
-import com.gabrieldutra.cursomc.domain.Categoria;
 
 @Service
 public class CategoriaService {
@@ -46,6 +49,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
